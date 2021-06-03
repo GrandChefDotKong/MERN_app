e298327dc6a170e8529f55dba1d2a41dee116f38
GrandChefDotKong <grandchefdotkong@gmail.com>
2021-06-03 15:28:52 +0900

    updated for deploy

--- server/index.js
+++ server/index.js
@@ -15,11 +15,11 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-app.get('/', (req, res) => res.send('APP Is RuNiNg'));
+app.get("/", (req, res) => { res.send("APP Is RuNiNg":)) };
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://adodotkong:LoZelda_64@cluster0.1mdlg.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) =>  console.log(error.message));
 
