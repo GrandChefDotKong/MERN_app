cb5f624c4d81316cfe56a3639a5fa35200486f85
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-02-28 14:41:52 +0900

    change server hosting

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URL, { 
     useNewUrlParser: true, useUnifiedTopology: true 
 })
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.then(() => app.listen(PORT, () => console.log(`Server running  ...`)))
 .catch((error) =>  console.log(error.message));
 
 
